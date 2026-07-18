33cfa8fa001b3662a6f1bf237081e55d89b8d259
sachinchaurasia2006-lgtm <306557352+sachinchaurasia2006-lgtm@users.noreply.github.com>
2026-07-19 00:50:07 +0530

    Apply dark teal interface theme

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,4 +3,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './premium.css'
+import './theme.css'
 createRoot(document.getElementById('root')).render(<StrictMode><App /></StrictMode>)
